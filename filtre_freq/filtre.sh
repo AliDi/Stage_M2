07e5e9d08e5a4f0e4f756907c89c9057ca5db552
AliDi <a.dinsen@hotmail.fr>
2016-05-09 18:33:02 +0200

    diverses petites modifs

--- filtre_freq/filtre.sh
+++ filtre_freq/filtre.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-if (($#==2))			#verification du nb d argument
+if (($#==3))			#verification du nb d argument
 then
 	
 	########## sauvegarde des donnees originales ##########
-	cd ./data	
+	#cd ./data	
 	for i in fsismos_P????
  		do cp $i filtre_${i}
 	done
 	
-	cd ..	
+	cp fricker filtre_fricker
+	
 	########## filtrage a la frequence $1 ##########
-	echo -e "$1 \n $2 \n" | octave filtre_freq.m
+	echo -e "$1 \n $2 \n $3 \n" | octave --silent --no-window-system filtre_freq.m
 
 	########## Affichage des donnees filtrees pour le couple 0-0 ##########
-	n1=` wc ./data/fsismos_P0000 | awk '{print $3}'` 
+	n1=` wc fsismos_P0000 | awk '{print $3}'` 
 	n2=$(($n1/$2/4))
-	ximage < ./data/fsismos_P0000 n1=$n2 perc=99 title='Donnees mesurees' &
-	ximage < ./data/filtre_fsismos_P0000 n1=$n2 perc=99 title='Donnees filtrees' &
-
+	ximage < fsismos_P0000 n1=$n2 perc=99 title='Donnees mesurees' &
+	ximage < filtre_fsismos_P0000 n1=$n2 perc=99 title='Donnees filtrees' &
+	
+	########## Affichage des excitations ##########
+	ximage < fricker n1=1 title='excitation originale'	&
+	ximage < filtre_fricker n1=1 title='excitation filtree' &
+	
 else
-  echo "Indique la frequence centrale du filtre et le nombre de sources "
+  echo "Indique la frequence centrale du filtre, le nombre de sources et le nombre de coefficient b pour filtre FIR"
 fi
 
 exit 0
